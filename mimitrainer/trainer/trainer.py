187b5e534856e1bafb12c4820e069c92264eac3b
phongtintruong <trung.nt204857@sis.hust.edu.vn>
2024-12-09 15:12:10 +0700

    refactor: fix type hint for feature extractor

--- mimitrainer/trainer/trainer.py
+++ mimitrainer/trainer/trainer.py
@@ -64,7 +64,7 @@ class MimiTrainer(nn.Module):
     def __init__(
         self,
         generator: PreTrainedModel,
-        feature_extractor: AutoFeatureExtractor,
+        feature_extractor: EncodecFeatureExtractor,
         discriminators: dict,
         cfg,
         accelerate_kwargs: dict = dict(),
