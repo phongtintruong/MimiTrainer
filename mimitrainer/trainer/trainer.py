5bb5d34e64fe920a291deb49e7c7cf6f09952b25
phongtintruong <trung.nt204857@sis.hust.edu.vn>
2024-12-09 15:48:47 +0700

    refactor: fix dl audio dim

--- mimitrainer/trainer/trainer.py
+++ mimitrainer/trainer/trainer.py
@@ -331,8 +331,8 @@ class MimiTrainer(nn.Module):
                 tic = time.time()
                 
                 x, semantic_feature = batch
+                x = x.squeeze().numpy()
                 print(x.shape)
-                x = x.cpu()
                 # x = x.unsqueeze(1)
                 # x = x.squeeze().numpy()
                 inputs = self.feature_extractor(raw_audio=x, sampling_rate=24000, return_tensors='pt').to(self.device)
@@ -389,10 +389,10 @@ class MimiTrainer(nn.Module):
                     with torch.inference_mode():
                         for i, batch in tqdm(enumerate(self.valid_dl)):                       
                             x, semantic_feature = batch
+                            x = x.squeeze().numpy()
                             print(x.shape)
                             # x = x.unsqueeze(1)
                             # x = x.squeeze().numpy()
-                            x = x.cpu()
 
                             inputs = self.feature_extractor(raw_audio=x,
                                                             sampling_rate=24000,
